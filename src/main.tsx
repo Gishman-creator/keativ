1fef68f5d2dabd9ccefd86e6e64127126fcab021
Gishman-creator <147145228+Gishman-creator@users.noreply.github.com>
2025-07-25 14:28:13 +0200

    added the favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import favicon from './public/logo.png';
+
+const faviconLink = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (faviconLink) {
+  faviconLink.href = favicon;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
